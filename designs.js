f480ee1697761f951a2f617a575eb05e61ffa3da
always42nate <40600820+always42nate@users.noreply.github.com>
2018-06-29 21:12:15 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,31 +3,34 @@
 
 // When size is submitted by the user, call makeGrid()
 
-var rowsInput;
-var colsInput;
 
-//function building the grids
-function makeGrid() {
-    var table = document.getElementById("pixelCanvas");
-//resets table ,when the submit button is clicked 
-    while(table.rows.length > 0)
-        table.deleteRow(0);
-    rowsInput = document.getElementById("inputHeight").value;
-    colsInput = document.getElementById("inputWeight").value;
-    for (var i = 0; i < rowsInput; i++) {
-        var row_elem = table.insertRow(i);
-        row_elem.setAttribute("class", "row");
-        for (var j = 0; j < colsInput; j++) {
-            var cell= row_elem.insertCell(j);
-            cell.addEventListener('click', function(evt) {
-                evt.target.style.backgroundColor = document.getElementById("colorPicker").value;
-            });
-        }
-    }
-    return false;
-}
 
-document.getElementById("sizePicker").addEventListener("submit", function(evt) {
-    evt.preventDefault();
+// Your code goes here!
+
+let color,height,width;
+$("#sizePicker").submit(function(e){
+    e.preventDefault();
+    //call make grid()
     makeGrid();
 });
+function makeGrid() {
+    $('tr').remove();
+    //get Grid sizes
+    height = $('#inputHeight').val();
+    width = $('#inputWeight').val();
+    for (let r = 1; r <= height; r++) {
+        $("#pixelCanvas").append('<tr id=table' + r + '></tr>');
+        for (let c = 1; c <= width; c++) {
+            $("#table" + r).append('<td></td>');
+        }
+    }
+    $('td').click(function addColor(){
+    //select your color value
+    color = $('#colorPicker').val();
+        if($(this).attr('style')){
+            $(this).removeAttr('style');
+        }else{
+            $(this).attr('style', 'background-color:' + color);
+        }
+    });
+}
